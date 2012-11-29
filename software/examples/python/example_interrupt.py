36f5346d9a5bf9fb84dbecead4a5ad4b6c9e1370
Olaf Lüke <olaf@tinkerforge.com>
2012-11-29 10:10:15 +0100

    Make examples compatible to Protocol V2.0

--- software/examples/python/example_interrupt.py
+++ software/examples/python/example_interrupt.py
@@ -15,11 +15,11 @@ def cb_interrupt(port, interrupt_mask, value_mask):
     print('Value: ' + str(bin(value_mask)))
 
 if __name__ == "__main__":
-    ipcon = IPConnection(HOST, PORT) # Create IP connection to brickd
+    ipcon = IPConnection() # Create IP connection
+    io = IO16(UID, ipcon) # Create device object
 
-    io = IO16(UID) # Create device object
-    ipcon.add_device(io) # Add device to IP connection
-    # Don't use device before it is added to a connection
+    ipcon.connect(HOST, PORT) # Connect to brickd
+    # Don't use device before ipcon is connected
 
     # Register callback for interrupts
     io.register_callback(io.CALLBACK_INTERRUPT, cb_interrupt)
@@ -28,4 +28,3 @@ if __name__ == "__main__":
     io.set_port_interrupt('a', 1 << 2)
 
     raw_input('Press key to exit\n') # Use input() in Python 3
-    ipcon.destroy()
--- software/examples/python/example_output.py
+++ software/examples/python/example_output.py
@@ -9,11 +9,11 @@ from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_io16 import IO16
 
 if __name__ == "__main__":
-    ipcon = IPConnection(HOST, PORT) # Create IP connection to brickd
+    ipcon = IPConnection() # Create IP connection
+    io = IO16(UID, ipcon) # Create device object
 
-    io = IO16(UID) # Create device object
-    ipcon.add_device(io) # Add device to IP connection
-    # Don't use device before it is added to a connection
+    ipcon.connect(HOST, PORT) # Connect to brickd
+    # Don't use device before ipcon is connected
 
     # Set pin 0 on port a to output low
     io.set_port_configuration('a', 1 << 0, 'o', False)
@@ -22,4 +22,3 @@ if __name__ == "__main__":
     io.set_port_configuration('b', (1 << 0) | (1 << 7), 'o', True)
 
     raw_input('Press key to exit\n') # Use input() in Python 3
-    ipcon.destroy()
