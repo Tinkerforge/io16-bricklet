78caeded75e73abb9c0edd567136f0e69186346f
Matthias Bolte <matthias@tinkerforge.com>
2013-08-16 10:50:27 +0200

    shell: Fix syntax error

--- software/examples/shell/example-output.sh
+++ software/examples/shell/example-output.sh
@@ -5,7 +5,7 @@
 uid=XYZ
 
 # set pin 0 on port a to output low: (1 << 0) = 1
-tinkerforge call io16-bricklet $uid set-port-configuration a 1 output false
+tinkerforge call io16-bricklet $uid set-port-configuration a 1 out false
 
 # set pin 0 and 7 on port b to output high: (1 << 0) | (1 << 7) = 129
-tinkerforge call io16-bricklet $uid set-port-configuration b 129 output true
+tinkerforge call io16-bricklet $uid set-port-configuration b 129 out true
