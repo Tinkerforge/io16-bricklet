605b4b269e766742ff81b9ef11b0dd2d4438a12e
Matthias Bolte <matthias@tinkerforge.com>
2015-08-12 11:21:33 +0200

    Fix minor warnings in C/C++ examples

--- software/examples/c/example_interrupt.c
+++ software/examples/c/example_interrupt.c
@@ -16,7 +16,7 @@ void cb_interrupt(char port, uint8_t interrupt_mask, uint8_t value_mask, void *u
 	printf("Value: %d\n", value_mask);
 }
 
-int main() {
+int main(void) {
 	// Create IP connection
 	IPConnection ipcon;
 	ipcon_create(&ipcon);
@@ -28,7 +28,7 @@ int main() {
 	// Connect to brickd
 	if(ipcon_connect(&ipcon, HOST, PORT) < 0) {
 		fprintf(stderr, "Could not connect\n");
-		exit(1);
+		return 1;
 	}
 	// Don't use device before ipcon is connected
 
@@ -44,4 +44,5 @@ int main() {
 	printf("Press key to exit\n");
 	getchar();
 	ipcon_destroy(&ipcon); // Calls ipcon_disconnect internally
+	return 0;
 }
