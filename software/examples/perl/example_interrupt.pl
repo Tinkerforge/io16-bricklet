65e5f10a18c4aa3c06c39936d36f8e47769d4fa4
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 15:53:06 +0200

    Cleanup Perl examples

--- software/examples/perl/example_interrupt.pl
+++ software/examples/perl/example_interrupt.pl
@@ -1,14 +1,14 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickletIO16;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => 'gfR'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
-my $io = Tinkerforge::BrickletIO16->new(&UID, $ipcon); # Create device object
+my $io16 = Tinkerforge::BrickletIO16->new(&UID, $ipcon); # Create device object
 
 # Callback function for interrupts
 sub cb_interrupt
@@ -24,12 +24,11 @@ $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
 # Register callback for interrupts
-$io->register_callback($io->CALLBACK_INTERRUPT, 'cb_interrupt');
+$io16->register_callback($io16->CALLBACK_INTERRUPT, 'cb_interrupt');
 
 # Enable interrupt on pin 2 of port a
-$io->set_port_interrupt('a', 1 << 2);
+$io16->set_port_interrupt('a', 1 << 2);
 
 print "Press any key to exit...\n";
 <STDIN>;
 $ipcon->disconnect();
-
--- software/examples/perl/example_output.pl
+++ software/examples/perl/example_output.pl
@@ -1,25 +1,24 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickletIO16;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => 'gfR'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
-my $io = Tinkerforge::BrickletIO16->new(&UID, $ipcon); # Create device object
+my $io16 = Tinkerforge::BrickletIO16->new(&UID, $ipcon); # Create device object
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
 # Set pin 0 on port a to output low
-$io->set_port_configuration('a', 1 << 0, 'o', 0);
+$io16->set_port_configuration('a', 1 << 0, 'o', 0);
 
 # Set pin 0 and 7 on port b to output high
-$io->set_port_configuration('b', (1 << 0) | (1 << 7), 'o', 1);
+$io16->set_port_configuration('b', (1 << 0) | (1 << 7), 'o', 1);
 
 print "Press any key to exit...\n";
 <STDIN>;
 $ipcon->disconnect();
-
