75861d923537fbb02a754133ff31561dec4371cc
Matthias Bolte <matthias@tinkerforge.com>
2015-09-30 10:00:21 +0200

    Improve callback handling in shell example

--- software/examples/shell/example-interrupt.sh
+++ software/examples/shell/example-interrupt.sh
@@ -3,8 +3,12 @@
 
 uid=XYZ # Change to your UID
 
+# Handle incoming interrupt callbacks
+tinkerforge dispatch io16-bricklet $uid interrupt &
+
 # Enable interrupt on pin 2 of port A: 1 << 2 = 4
 tinkerforge call io16-bricklet $uid set-port-interrupt a 4
 
-# Handle incoming interrupt callbacks
-tinkerforge dispatch io16-bricklet $uid interrupt
+echo "Press key to exit"; read dummy
+
+kill -- -$$ # Stop callback dispatch in background
