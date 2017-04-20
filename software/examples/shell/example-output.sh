d51d98332290ad6763f26eb737a8b5ee97236699
Matthias Bolte <matthias@tinkerforge.com>
2017-04-20 19:32:16 +0200

    shell: Adapt example

--- software/examples/shell/example-output.sh
+++ software/examples/shell/example-output.sh
@@ -4,7 +4,7 @@
 uid=XYZ # Change XYZ to the UID of your IO-16 Bricklet
 
 # Set pin 0 on port A to output low: 1 << 0 = 1
-tinkerforge call io16-bricklet $uid set-port-configuration a 1 out false
+tinkerforge call io16-bricklet $uid set-port-configuration a 1 direction-out false
 
 # Set pin 0 and 7 on port B to output high: (1 << 0) | (1 << 7) = 129
-tinkerforge call io16-bricklet $uid set-port-configuration b 129 out true
+tinkerforge call io16-bricklet $uid set-port-configuration b 129 direction-out true
