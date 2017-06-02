be1081df960c0c8e27b3fb06f1dd20494a0e48f4
Matthias Bolte <matthias@tinkerforge.com>
2017-06-02 16:14:17 +0200

    octave: Use javaObject instead of deprecated java_new

--- software/examples/matlab/octave_example_interrupt.m
+++ software/examples/matlab/octave_example_interrupt.m
@@ -5,8 +5,8 @@ function octave_example_interrupt()
     PORT = 4223;
     UID = "XYZ"; % Change XYZ to the UID of your IO-16 Bricklet
 
-    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
-    io = java_new("com.tinkerforge.BrickletIO16", UID, ipcon); % Create device object
+    ipcon = javaObject("com.tinkerforge.IPConnection"); % Create IP connection
+    io = javaObject("com.tinkerforge.BrickletIO16", UID, ipcon); % Create device object
 
     ipcon.connect(HOST, PORT); % Connect to brickd
     % Don't use device before ipcon is connected
