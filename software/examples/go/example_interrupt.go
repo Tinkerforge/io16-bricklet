6c90ef0dc8c01d863f14e0b57827472bc57587a9
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 14:55:44 +0100

    go: Change import path.

--- software/examples/go/example_interrupt.go
+++ software/examples/go/example_interrupt.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"tinkerforge/io16_bricklet"
-	"tinkerforge/ipconnection"
+	"github.com/tinkerforge/go-api-bindings/io16_bricklet"
+	"github.com/tinkerforge/go-api-bindings/ipconnection"
 )
 
 const ADDR string = "localhost:4223"
